a5d82974b690d7b831dba97512cc4cd9f418500d
TuobaZhen <2459769486@qq.com>
2024-06-13 18:20:46 +0800

    新建LeNet

--- cnn/mnist_dnn.py
+++ cnn/mnist_dnn.py
@@ -20,8 +20,8 @@ transform=transforms.Compose([
     transforms.Normalize((0.1307),(0.3081))#让图片数据做标准归一化;     两个参数分别是是标准归一化的均值和方差
 ])
 # 读取数据
-datasets1=datasets.MNIST('./data',train=True,download=True,transform=transform)#训练的数据集
-datasets2=datasets.MNIST('./data',train=False,download=True,transform=transform)#不是训练的数据集
+datasets1=datasets.MNIST('../pytorch_study/data', train=True, download=True, transform=transform)#训练的数据集
+datasets2=datasets.MNIST('../pytorch_study/data', train=False, download=True, transform=transform)#不是训练的数据集
 
 
 #设置数据的加载器,,,顺便设置批次大小和是否打乱顺讯
@@ -53,33 +53,13 @@ class Net(nn.Module):
         super(Net,self).__init__()
         # 784表示上一层有多少个维度,,128表示当前隐藏层有多少个神经元
         #注意这里的nn.Linear只是一个∑,,,还没有引入非线性变换
-        self.fc1=nn.Linear(1600,128)
+        self.fc1=nn.Linear(784,128)
 
         # 参数表示 drop_rate
         self.dropout=nn.Dropout(0.2)
         self.fc2=nn.Linear(128,10)
 
-        # 第一个参数表示输入的通道数,,由于是黑白图所以是1
-        # 第二个参数选择输出多少个通道,其实就是选多少个卷积核
-        # 第三个参数表示卷积核的大小  第四个表示步长
-
-        self.conv1=nn.Conv2d(1,64,3,1)
-        # 经过第一个卷积层后得到的FeatureMap是64,因此第二个卷积层的输入是64
-        #Tip:池化不改变通道数
-        self.conv2=nn.Conv2d(64,64,3,1)
-
     def forward(self,x):
-        x=self.conv1(x)
-        x=F.relu(x)
-        # 表示池化核的大小是2*2   步长默认和池化核一致,,,也是2*2的
-        x=F.max_pool2d(x,2)
-
-        x=self.conv2(x)
-        x=F.relu(x)
-        x=F.max_pool2d(x,2)
-
-        # 对于一般的CNN框架来说,下一步应该把数据从4D转化为2D,,下面的表示从3D转化为1D
-        # (这里的转化可以理解为把RGB转为一维向量,3D->1D表示1个RGB->一维向量,4D->2D就是多个RGB->多个一维向量,,)
         # 把每条数据变成一维数组
         x=torch.flatten(x,1)
         # 经过一个全连接
@@ -87,7 +67,6 @@ class Net(nn.Module):
         x=self.fc1(x)
         #F.relu 表示一个非线性变换     两个函数拼接起来就表示经过了第一层全连接
         x=F.relu(x)
-
         x=self.dropout(x)
         x=self.fc2(x)
         output=F.log_softmax(x,dim=1)
@@ -100,21 +79,14 @@ model=Net().to(device)
 #定义训练模型的逻辑
 def train_step(data,target,model,optimizer):
     optimizer.zero_grad()
-
-    
     # 把数据交给模型,让它跑一个正向传播
     output=model(data)
     #nll 表示 negative log likely hood  负对数似然
     # out_put就是y预测,,,target就是真实值
     # 如果传入了一个批次的数据,,那么当前算的就是一个批次的损失
     loss=F.nll_loss(output,target)
-
     #进行一个反向传播,,,反向传播的本质是求梯度
     loss.backward()
-
-
-
-
     #反向传播的目的就是调参,,,接下来用optimizer来调参
     optimizer.step()
     return loss
